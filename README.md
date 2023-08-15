9c0f8ca846c2d2c46d4c6b345397d87593b06b00
loanerror <maroonjohns@pm.me>
2023-08-15 22:22:11 +0800

    🔑 Added AWS_SECRET_ACCESS_KEY

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+## set AWS
 
+1. `cp secret.tfvars secret.auto.tfvars`
+2. edit `secret.auto.tfvars`
--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ terraform {
 
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
